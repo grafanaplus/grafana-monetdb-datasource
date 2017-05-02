2ed111b706bd945a3929be6389cfd9158ccc645d
sstalin <svetlin.stalinov@monetdbsolutions.com>
2017-05-02 17:44:04 +0200

    adding nnotations test

---
+++ src/specs/monet_series.spec.ts
@@ -0,0 +1,31 @@
+import MonetSeries from '../monet_series';
+
+describe('When generating Grafana data from MonetDB quesry response,', () => {
+
+    describe('given annotation repsponse', () => {
+        let serie = {
+            values: [
+                [1451607290, 100]
+            ]
+        };
+        let option = {
+            annotation: {
+                tagsColumn: 'datacenter,source',
+                titleColumn: "",
+                textColumn: 'text'
+            },
+        };
+        let ms = new MonetSeries(serie, option);
+
+        it('should handle tags correctly', () => {
+            let anotations = ms.getAnnotations();
+            expect(anotations[0].tags.length).toBe(2);
+        });
+
+        it('should substitue title with the value when no title', () => {
+            let anotations = ms.getAnnotations();
+            expect(anotations[0].title).toBe(100);
+        });
+
+    });
+});
